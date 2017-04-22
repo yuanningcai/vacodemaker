2ae8a06c529e304434183e169e4ef12b7fa9ef85
yuanningcai <wulinqunxia2004@163.com>
2017-04-22 13:20:10 +0800

    make.sh

---
+++ make.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+app_home=/home/admin/Project/vacodemaker
+lib_path=$app_home/lib
+src_path=$app_home/src/main/java
+
+jars=.:$lib_path/patchca-0.5.0.jar
+srcs=$src_path/nb/vacodemaker/bootstrap.java
+
+function build()
+{
+        javac -classpath $jars $srcs
+}       
+
+function run()
+{
+        cd $src_path
+        java  -classpath $jars nb.vacodemaker.bootstrap
+}       
+
+build;
+run;
